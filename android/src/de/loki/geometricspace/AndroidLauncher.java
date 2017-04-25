6c8f5123e58f4f731497b980b1b9842371a2f864
LokiGodofBattle <Loki_GodofBattle@gmx.de>
2017-04-25 19:13:03 +0200

    Canvas Setup

---
+++ android/src/de/loki/geometricspace/AndroidLauncher.java
@@ -0,0 +1,20 @@
+package de.loki.geometricspace;
+
+import android.os.Bundle;
+import android.view.WindowManager;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import de.loki.geometricspace.Main;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		config.useImmersiveMode = true;  //LG Optionen-Leiste ausblenden z.B.
+		config.numSamples = 2;  //Antialysing
+		getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);  //Ausschalten des Screens verhindern
+		initialize(new Main(), config);
+	}
+}
---
+++ core/src/de/loki/geometricspace/Main.java
@@ -0,0 +1,60 @@
+package de.loki.geometricspace;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+
+public class Main extends ApplicationAdapter {
+	SpriteBatch batch;
+	private static OrthographicCamera camera;
+	private Viewport viewport;
+	public final static int VIEWPORT_WIDTH = 1440;
+	private static float aspect_ratio;
+
+	@Override
+	public void create() {
+		batch = new SpriteBatch();
+
+		//Ausrechnen des Größenverhältnisses des Geräts
+		aspect_ratio = Gdx.graphics.getHeight() / (float) Gdx.graphics.getWidth();
+
+		camera = new OrthographicCamera();
+
+		//FitViewport damit es auf allen Geräten gleich aussieht
+		viewport = new FitViewport(VIEWPORT_WIDTH, VIEWPORT_WIDTH * aspect_ratio, camera);
+		viewport.apply();
+
+		//Kamera zentrieren
+		camera.position.set(VIEWPORT_WIDTH / 2f, VIEWPORT_WIDTH * aspect_ratio / 2f, 0);
+
+	}
+
+	@Override
+	public void render() {
+		Gdx.gl.glClearColor(0, 0, 0, 0);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		batch.begin();
+		batch.end();
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		super.resize(width, height);
+		viewport.update(width, height);
+		camera.position.set(VIEWPORT_WIDTH / 2f, VIEWPORT_WIDTH * aspect_ratio / 2f, 0);
+	}
+
+	public static float getViewportHeight() {
+		return VIEWPORT_WIDTH * aspect_ratio;
+	}
+}
