e491623440e5839fbb3fb5fd8907cbe597ef42b3
LokiGodofBattle <Loki_GodofBattle@gmx.de>
2017-05-14 16:37:42 +0200

    added DragForce

--- core/src/de/loki/geometricspace/Physics.java
+++ core/src/de/loki/geometricspace/Physics.java
@@ -1,5 +1,6 @@
 package de.loki.geometricspace;
 
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
 
 /**
@@ -9,26 +10,53 @@ import com.badlogic.gdx.math.Vector2;
 public class Physics {
 
     private static float gravity;
+    private static float dragForce;
+    private static float maxLength;
 
     public static void init(){
-        gravity = 5000;
+        gravity = 30000;
+        dragForce = 0.01f;
+        maxLength = 750;
     }
 
     public static void render(){
 
-        for(int i = 0; i<Main.particles.size; i++)
+        applyGravity();
+
+    }
+
+    private static void applyGravity(){
+        for(Particle p : Main.particles)
         {
             Vector2 acceleration = Player.getPosition().cpy();
-            acceleration.sub(Main.particles.get(i).getPosition().cpy());
+            acceleration.sub(p.getPosition().cpy());
 
+            if(acceleration.len() <= maxLength){
+                float length = acceleration.len();
 
-            float force = (float) (gravity/ Math.pow((double) acceleration.len(), 2));
+                float force = (float) (gravity/ Math.pow(length, 2) *1000);
 
-            acceleration.nor();
-            acceleration.setLength(force*1000);
+                acceleration.nor();
+                acceleration.setLength(force);
 
-            Main.particles.get(i).applyForce(acceleration);
+                p.applyForce(acceleration);
+            } else {
+                applyDrag(p);
+            }
+
         }
+    }
+
+    private static void applyDrag(Particle p){
+
+            float force = dragForce * p.getVelocity().len() * p.getVelocity().len();
+
+            Vector2 drag = p.getVelocity().cpy();
+            drag.nor();
+            drag.scl(force);
+            drag.scl(-1);
+
+            p.applyForce(drag);
     }
 
 }
